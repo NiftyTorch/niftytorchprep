56f8cb353d8ae18c3786e3a5446e7075fc8aff7a
Dominik Krzeminski <dominik@appsilon.com>
2020-06-17 15:48:37 +0100

    added functional and way of installing tool

--- niftytorchprep/__main__.py
+++ niftytorchprep/__main__.py
@@ -8,21 +8,39 @@ def main():
         print("You are probably running `niftytorchprep` for the first time")
         print("Press [enter] to install pyinvoke or Ctrl+C to abort\n")
         input()
-        os.system("sudo pip install invoke==1.4.1")
+        os.system("pip install invoke==1.4.1")
         raise
 
     @task
-    def qc_t1(c, bids_dir, out_dir = None):
+    def getvisualqc(c):
         """
-        Runs Quality control from visualqc package and store the
+        Installs visualqc.
+        """
+        c.run("pip install -U visualqc", echo = True)
+
+    @task
+    def qct1(c, bids_dir, out_dir = None):
+        """
+        Runs Quality Control (T1) from visualqc package and stores the
         results in the *out_dir*
         """
         command = f"visualqc_t1_mri -b {bids_dir}"
         if not out_dir is None:
             command += " -o {out_dir}"
+        out = c.run(command, echo = True)
+
+    @task
+    def qcfunc(c, bids_dir, out_dir = None):
+        """
+        Runs Quality Control (Functional) from visualqc package and stores the
+        results in the *out_dir*
+        """
+        command = f"visualqc_func_mri -b {bids_dir}"
+        if not out_dir is None:
+            command += " -o {out_dir}"
         c.run(command, echo = True)
 
-    namespace = Collection(qc_t1)
+    namespace = Collection(qct1, qcfunc, getvisualqc)
 
     program = Program(
       version = "0.0.1",
