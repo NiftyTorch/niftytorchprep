7955a8f22d7490fd51fc6117a8dd1b4eecdc83dc
Dominik Krzeminski <dominik@appsilon.com>
2020-06-18 11:42:06 +0100

    cli tool updated

--- niftytorchprep/__main__.py
+++ niftytorchprep/__main__.py
@@ -1,20 +1,41 @@
 import click
 import os
-from subprocess import PIPE, run
-import invoke
+import glob
+import re
+from invoke import run
 
-def out(command):
-    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    print(result.stdout)
-    print(result.stderr)
-    return result.stdout, result.stderr
+from .tools import *
+
+@click.group("BIDS control")
+def bids():
+    pass
+
+@bids.command()
+@click.argument('bids_dir', type = click.Path(exists = True))
+def bids_validate(bids_dir):
+    """
+    Basic BIDS verification. Print 'ok' for correct BIDS files or 'no' otherwise.
+    """
+    check_bids_files(bids_dir)
+
+@bids.command()
+@click.argument('bids_dir', type = click.Path(exists = True))
+def bids_files(bids_dir):
+    """
+    Print types of files and their number per folder.
+    """
+    list_bids_files(bids_dir)
+
+@click.group("Data preparation")
+def data():
+    pass
 
 @click.group("Quality Control")
 def qc():
     pass
 
 @qc.command()
-@click.argument('bids_dir', type=click.Path(exists=True))
+@click.argument('bids_dir', type = click.Path(exists = True))
 @click.option('--out_dir', default=None, help="output directory")
 def qc_anat(bids_dir, out_dir):
     """
@@ -24,12 +45,12 @@ def qc_anat(bids_dir, out_dir):
     command = f"visualqc_t1_mri -b {bids_dir}"
     if not out_dir is None:
         command += " -o {out_dir}"
-    output, err = out(command)
-    if "command not found" in err:
+    out = run(command, echo = True, warn = True)
+    if out.failed == True and "command not found" in out.sterr:
         print("You should install *visualqc* first")
 
 @qc.command()
-@click.argument('bids_dir', type=click.Path(exists=True))
+@click.argument('bids_dir', type = click.Path(exists = True))
 @click.option('--out_dir', default=None, help="output directory")
 def qc_func(bids_dir, out_dir):
     """
@@ -39,19 +60,19 @@ def qc_func(bids_dir, out_dir):
     command = f"visualqc_func_mri -b {bids_dir}"
     if not out_dir is None:
         command += " -o {out_dir}"
-    output, err = out(command)
-    if "command not found" in err:
+    out = run(command, echo = True, warn = True)
+    if out.failed == True and "command not found" in out.sterr:
         print("You should install *visualqc* first")
 
 @qc.command()
-def qc_visualqc():
+def qc_getvisualqc():
     """
     Installs visualqc from PIP.
     """
-    invoke.run("pip install -U visualqc")
+    run("pip install -U visualqc")
 
 
-cli = click.CommandCollection(sources=[qc])
+cli = click.CommandCollection(sources=[qc, bids])
 
 if __name__ == '__main__':
     cli()
